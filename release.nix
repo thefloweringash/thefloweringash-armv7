983635f6ae48b93f833f2ddd4d052e9def7213e8
Andrew Childs <lorne@cons.org.nz>
2020-01-15 23:24:53 +0900

    Expand to an optimistic set of packages

--- release.nix
+++ release.nix
@@ -6,5 +6,35 @@ let
   };
 in
 {
-  inherit (pkgs) hello;
+  inherit (pkgs)
+    # essentials
+    stdenv
+
+    # sanity check
+    hello
+
+    # common interpreters
+    ghc
+    go
+    perl
+    python3
+    ruby
+    rustc
+
+    # commonly used tools
+    file
+    gdb
+    git
+    mtdutils
+    ncdu
+    neofetch
+    pandoc
+    sysstat
+    tcpdump
+    tio
+    tmux
+    tree
+    vim
+    xterm
+    ;
 }
