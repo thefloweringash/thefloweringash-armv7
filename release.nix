87e78bbeda186295d11b0d27f93a6a3977dc37ef
Andrew Childs <lorne@cons.org.nz>
2020-01-18 18:54:08 +0900

    Keep adding things

--- release.nix
+++ release.nix
@@ -11,6 +11,7 @@ in
     bash
     binutils
     busybox
+    bzip2
     coreutils
     curl
     gettext
@@ -25,17 +26,28 @@ in
     # sanity check
     hello
 
-    # common interpreters and compilers
+    # common interpreters, compilers and dev tools
+    autoconf
+    automake
+    bison
     cargo
     clang
+    expect
+    flex
     ghc
     go
+    guile
+    help2man
     llvm
     lua
     perl
+    python2
     python3
     ruby
     rustc
+    swig
+    tcl
+    tk
 
     # common build systems and tools
     cmake
@@ -44,20 +56,40 @@ in
     pkg-config
 
     # commonly used tools
+    asciidoc
     file
     gdb
     git
+    gnupg
     mtdutils
     ncdu
     neofetch
     pandoc
+    sqlite
     sysstat
     tcpdump
     tio
     tmux
     tree
+    unzip
     vim
     xterm
+
+    # common libraries
+    db
+    expat
+    gmp
+    isl
+    libelf
+    libffi
+    libmpc
+    libxml2
+    libxslt
+    mpfr
+    ncurses
+    npth
+    pcre
+    readline
 
     # kernels
     linux
